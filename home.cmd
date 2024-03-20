a97dd5e3341470be5cccd02130d8942d7c04677e
SiKr <6181231+sikr@users.noreply.github.com>
2024-03-20 14:56:54 +0100

    Aliases moved to conf

--- home.cmd
+++ home.cmd
@@ -1,3 +1,5 @@
 @echo off
-doskey /macrofile=%HOME%\scripts\aliases.txt
+IF EXIST %HOME%\conf\windows\.aliases (
+  doskey /macrofile=%HOME%\conf\windows\.aliases
+)
 call "C:\Program Files (x86)\clink\clink-term.bat" --quiet
